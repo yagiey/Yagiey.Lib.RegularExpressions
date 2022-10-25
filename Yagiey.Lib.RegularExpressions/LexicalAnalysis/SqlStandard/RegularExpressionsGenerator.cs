6e3a6ce491c60f30a4a92dedef36c467a6ffab71
yagiey <yagiey@gmail.com>
2022-10-25 21:58:37 +0900

    added SQL style comment

---
+++ Yagiey.Lib.RegularExpressions/LexicalAnalysis/SqlStandard/RegularExpressionsGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Yagiey.Lib.RegularExpressions.Automata;
+using Yagiey.Lib.RegularExpressions.Functions;
+
+namespace Yagiey.Lib.RegularExpressions.LexicalAnalysis.SqlStandard
+{
+	using DFA = IDeterministicFiniteAutomaton<char>;
+	using DFATransitionMap = IDictionary<int, IDictionary<IInput, int>>;
+
+	public class RegularExpressionsGenerator
+	{
+		public static DFA LineComment()
+		{
+			DFATransitionMap transitionMap = new Dictionary<int, IDictionary<IInput, int>>
+			{
+				{
+					0,
+					new Dictionary<IInput, int> {
+						{ new Input('-'), 1 },
+					}
+				},
+				{
+					1,
+					new Dictionary<IInput, int> {
+						{ new Input('-'), 2 },
+					}
+				},
+				{
+					2,
+					new Dictionary<IInput, int> {
+						{ new Input('\r'), 3 },
+						{ new Input('\n'), 4 },
+						{ new InputWithPredicate(
+							new And<char>(
+								new List<IUnaryFunctionObject<bool,char>>
+								{
+									new Not<char>(new Equal<char>('\r')),
+									new Not<char>(new Equal<char>('\n')),
+								}
+							)
+						), 2 },
+					}
+				},
+				{
+					3,
+					new Dictionary<IInput, int> {
+						{ new Input('\n'), 4 },
+						{ new InputWithPredicate(new Not<char>(new Equal<char>('\n'))), 2 },
+					}
+				},
+			};
+			return new RegularExpression(0, new int[] { 4 }, transitionMap, false);
+		}
+	}
+}
