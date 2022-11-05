5a9e10232bf441a876905853de4dee2dda6986eb
yagiey <yagiey@gmail.com>
2022-11-05 11:29:20 +0900

    added constructors which take arguments

--- Yagiey.Lib.RegularExpressions/LexicalAnalysis/Odbc/RegularExpressionsGenerator.cs
+++ Yagiey.Lib.RegularExpressions/LexicalAnalysis/Odbc/RegularExpressionsGenerator.cs
@@ -17,12 +17,31 @@ namespace Yagiey.Lib.RegularExpressions.LexicalAnalysis.Odbc
 			return new NegativeLookahead($"{pat1}", $"{pat2}", false);
 		}
 
+		public static DFA Date(StringAffix affix)
+		{
+			// {d '1995-01-15'}
+			var patterns = Iso8601.RegularExpressionsGenerator.GetDatePattern();
+			string pat1 = @$"\{{\s*(d|D)\s*'({patterns.Item1})";
+			string pat2 = @$"\{{\s*(d|D)\s*'({patterns.Item2})'\s*\}}";
+			return new NegativeLookahead(affix, pat1, pat2, false);
+		}
+
 		public static DFA Time()
 		{
 			// {t 'hh:mm:ss[.fractional seconds]'}
 			const int DigitsFsec = 7;
 			string pattern = Iso8601.RegularExpressionsGenerator.GetTimePattern(DigitsFsec);
 			return new RegularExpression(@$"\{{\s*(t|T)\s*'({pattern})'\s*\}}", false);
+		}
+
+		public static DFA Time(StringAffix affix)
+		{
+			// {t 'hh:mm:ss[.fractional seconds]'}
+			string prefix = Constants.Sanitize(affix.Prefix);
+			string suffix = Constants.Sanitize(affix.Suffix);
+			const int DigitsFsec = 7;
+			string pattern = Iso8601.RegularExpressionsGenerator.GetTimePattern(DigitsFsec);
+			return new RegularExpression(@$"{prefix}\{{\s*(t|T)\s*'({pattern})'\s*\}}{suffix}", false);
 		}
 
 		public static DFA DateTime()
@@ -34,5 +53,13 @@ namespace Yagiey.Lib.RegularExpressions.LexicalAnalysis.Odbc
 			return new NegativeLookahead(@$"\{{\s*((t|T)(s|S))\s*'{patternsDate.Item1}", @$"\{{\s*((t|T)(s|S))\s*'{patternsDate.Item2} {patternTime}'\s*\}}", false);
 		}
 
+		public static DFA DateTime(StringAffix affix)
+		{
+			// { ts 'yyyy-mm-dd hh:mm:ss[.fractional seconds]' }
+			const int DigitsFsec = 7;
+			var patternsDate = Iso8601.RegularExpressionsGenerator.GetDatePattern();
+			string patternTime = Iso8601.RegularExpressionsGenerator.GetTimePattern(DigitsFsec);
+			return new NegativeLookahead(affix, @$"\{{\s*((t|T)(s|S))\s*'{patternsDate.Item1}", @$"\{{\s*((t|T)(s|S))\s*'{patternsDate.Item2} {patternTime}'\s*\}}", false);
+		}
 	}
 }
